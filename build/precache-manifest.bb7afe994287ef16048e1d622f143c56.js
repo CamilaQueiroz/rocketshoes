8c227054cede505e9076c4639b30b8cf76460f71
Camila <camilasvqueiroz@gmail.com>
2019-11-11 21:13:28 -0300

    build

---
+++ build/precache-manifest.bb7afe994287ef16048e1d622f143c56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770005285c81324db7ecbbe02b7e8180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d4301bf01bc7ad11ebd",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "6d4301bf01bc7ad11ebd",
+    "url": "/static/js/2.3bc7df37.chunk.js"
+  },
+  {
+    "revision": "85e1af0532a4ac7fc8ab",
+    "url": "/static/js/main.66f51325.chunk.js"
+  },
+  {
+    "revision": "ad3e0a3e3be41594cdc2",
+    "url": "/static/js/runtime~main.af021c72.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/static/media/background.3c9a1ce5.svg"
+  },
+  {
+    "revision": "8be1cc4f785df71247bd73a4109bd3ac",
+    "url": "/static/media/loader.8be1cc4f.svg"
+  }
+]);
